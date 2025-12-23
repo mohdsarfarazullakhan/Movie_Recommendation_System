c5ba0854d2e538829a7bdf90abbae357388a919c
Md Sarfaraz Ulla Khan <mohdsarfarazullakhan@gmail.com>
2025-12-24 02:04:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import pickle
+import streamlit as st
+import requests
+
+# -----------------------------
+# Fetch poster from TMDB
+# -----------------------------
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=4c15b3b3425785aa0b4f18d6611c7f27&language=en-US"
+    response = requests.get(url)
+    data = response.json()
+
+    poster_path = data.get("poster_path")
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    return "https://via.placeholder.com/500x750?text=No+Image"
+
+# -----------------------------
+# Recommendation function
+# -----------------------------
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )
+
+    recommended_movie_names = []
+    recommended_movie_posters = []
+
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movie_posters.append(fetch_poster(movie_id))
+
+    return recommended_movie_names, recommended_movie_posters
+
+# -----------------------------
+# Streamlit UI
+# -----------------------------
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+st.header("🎬 Movie Recommender System")
+
+# Load data (USE RELATIVE PATHS)
+movies = pickle.load(open("movie_list.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button("Show Recommendation"):
+    names, posters = recommend(selected_movie)
+
+    cols = st.columns(5)
+    for col, name, poster in zip(cols, names, posters):
+        with col:
+            st.text(name)
+            st.image(poster)
